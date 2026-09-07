2e18cae3d138cd2ec42a509a68c8460f16a5c584
VigoWB <vigoar@tlen.pl>
2026-09-07 10:49:47 +0000

    fix: add base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://foto.vigolab.ovh',
+  base: '/',
   integrations: [sitemap()],
   output: 'static',
   vite: {

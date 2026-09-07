7c4e2099f8b10ae8af35a7a10af8a0460721baa3
VigoWB <vigoar@tlen.pl>
2026-09-07 09:56:44 +0000

    bugi wersji

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://foto.vigolab.ovh',
   integrations: [sitemap()],
-  adapter: cloudflare(),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
